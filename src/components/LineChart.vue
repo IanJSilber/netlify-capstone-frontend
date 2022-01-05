95cd6f16c4c02b55176ed6cf0e35cb707d3f414f
Ian S <iansilber32@gmail.com>
2022-01-05 18:17:17 -0500

    tests new math

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -63,14 +63,14 @@ export default {
           this.totalPnl30Days += this.positions[i].pnl_30_days;
         }
         if (this.totalPnl7Days > 0) {
-          this.totalPnl7Days -= this.totalValue;
+          this.totalPnl7Days = this.totalValue - this.totalPnl7Days;
         } else {
-          this.totalPnl7Days = Math.abs(this.totalPnl7Days) - this.totalValue;
+          this.totalPnl7Days = this.totalValue - Math.abs(this.totalPnl7Days);
         }
         if (this.totalPnl30Days > 0) {
-          this.totalPnl30Days -= this.totalValue;
+          this.totalPnl30Days = this.totalValue - this.totalPnl30Days;
         } else {
-          this.totalPnl30Days = Math.abs(this.totalPnl30Days) - this.totalValue;
+          this.totalPnl30Days = this.totalValue - Math.abs(this.totalPnl30Days);
         }
         this.totalPnl30Days += this.totalValue;
         this.chartData.datasets[0].data.push(this.totalPnl30Days);
