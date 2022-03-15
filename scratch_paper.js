0a27b03d1c99e693f474bb70a4564ea8dc21b28f
Ian S <iansilber32@gmail.com>
2022-03-14 22:12:17 -0400

    writing things down

--- scratch_paper.js
+++ scratch_paper.js
@@ -1,3 +1,5 @@
+// dont mind me just testing random things
+
 let values = [1, 2, 4, 6, 7, 7, 1, 3, 4];
 let array1 = values;
 let array2 = values;
@@ -6,3 +8,13 @@ array1.sort((a, b) => {
 });
 
 console.log(array1, array2);
+
+console.log("==================");
+
+let newValues = [1, 2, 4, 6, 7, 7, 1, 3, 4];
+
+let array3 = newValues;
+let array4 = [...newValues].sort(); // this works by making a copy of newValues preventing .sort from
+// mutating the original
+
+console.log(array3, array4);
